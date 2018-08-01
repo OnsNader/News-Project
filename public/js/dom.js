fafeb011dde40b6535a7012a7d5191f097c7c63d
knanahassouna1 <knanahassouna2@gmail.com>
2018-08-01 08:58:16 +0300

    Add Style and some javascript #4

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,21 +1,15 @@
-
-
 const button = document.getElementById('button');
 const search = document.getElementById('search');
 const date = document.getElementById('date');
 
-console.log(button);
-console.log(search);
-console.log(date);
 button.addEventListener('click', (e) => {
-    console.log(e.target);
-  const searchQuery = {
+ const data=getDataFromFields();
+  sendRequest(data);
+});
+
+function getDataFromFields(){
+  return  {
     query: search.value,
     date: date.value,
   };
-  fetch('POST', searchQuery, viewResult);
-});
-
-function viewResult(Items) {
-  console.log(Items);
 }
